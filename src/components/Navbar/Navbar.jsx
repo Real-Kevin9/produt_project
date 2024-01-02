aed9442f624ab32327f81ea14749ecb600151b10
kevin <kevinrajkarki97@gmail.com>
2024-01-02 08:33:16 +0545

    already added

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+export const Navbar = () => {
+    return( 
+        <header className='w-full'>
+            <div className="container mx-auto justify-between flex">
+            <div>Miralou</div>
+            <div>Links</div>
+            <div>Actions</div>
+            </div>
+        </header>
+    );
+};
